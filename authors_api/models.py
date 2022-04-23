7c9595647d2c38c3848b01c45d87884bc679de12
Pouria Nazemi <pouria_nazemi@mail.um.ac.ir>
2022-04-24 02:01:25 +0430

    creating user models

--- authors_api/models.py
+++ authors_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(null = False, blank = False)
+    blog = models.URLField(null = True, blank = True)
+
+    objects = UserManager()
