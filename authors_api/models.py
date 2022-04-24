05f14bdaaa7fe3b0c31956a876778bf0baa395e0
Pouria Nazemi <pouria_nazemi@mail.um.ac.ir>
2022-04-24 19:19:56 +0430

    Creating articles model

--- authors_api/models.py
+++ authors_api/models.py
@@ -7,3 +7,15 @@ class User(AbstractUser):
     blog = models.URLField(null = True, blank = True)
 
     objects = UserManager()
+
+class Article(models.Model):
+    title = models.CharField(max_length = 300,blank = False, null = False)
+    description = models.TextField(blank = False, null = False)
+    link = models.URLField(blank = False, null = False)
+    date_modified = models.DateField(auto_now = True)
+    subject = models.CharField(max_length = 50, blank = False, null = False)
+    author_id = models.ForeignKey(User, on_delete = models.CASCADE)
+
+
+
+    
