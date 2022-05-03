b92ff9129716ae1713e666332a3b4d207ee70d33
Pouria Nazemi <pouria_nazemi@mail.um.ac.ir>
2022-05-03 14:56:24 +0430

    Changing root url

--- ArticleBlog/urls.py
+++ ArticleBlog/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authors_api.urls'))
+    path('', include('authors_api.urls'))
 ]
