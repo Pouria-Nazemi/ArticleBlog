b5a5ca00714f076b245d1880ae03beb4b66ab32b
Pouria Nazemi <pouria_nazemi@mail.um.ac.ir>
2022-08-05 01:46:16 +0430

    fix a problem

--- ArticleBlog/settings.py
+++ ArticleBlog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd3bx83u1$(=1^5=!k1_ds50qo4f95a7tayn%@*i(1tj6y(9jj0'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
